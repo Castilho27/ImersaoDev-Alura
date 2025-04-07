f57797bbcbf1cb929b8134061dcfb706c8b2ff14
Lucas Castilho <lubertanhe@gmail.com>
2025-04-06 21:07:03 -0300

    Primeiro Projeto - Commit 1

---
+++ aula1/conversor_de_moeda/src/script.js
@@ -0,0 +1,29 @@
+function wons() {
+valor_wons = prompt("Digite um valor em Wons");
+  if (valor_wons) {
+      um_won = 0.0040
+      valor_final = valor_wons * um_won
+          alert(" O valor convertido em reais é R$" + valor_final) }
+  else {
+    alert("Por favor, insira um valor válido") }
+   }
+  
+function euros() {
+valor_euros = prompt("Digite um valor em Euros");
+  if (valor_euros) {
+      um_euro = 6.20
+      valor_final_e = valor_euros * um_euro
+          alert(" O valor convertido em reais é R$" + valor_final_e) }
+  else {
+    alert("Por favor, insira um valor válido") }
+   }
+
+function dollar() {
+valor_dollar= prompt("Digite um valor em Dollar");
+  if (valor_dollar) {
+      um_dollar = 5.60
+      valor_final_d = valor_dollar * um_dollar
+          alert(" O valor convertido em reais é R$" + valor_final_d) }
+  else {
+    alert("Por favor, insira um valor válido") }
+   }
